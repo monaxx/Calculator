bb9f2102745b49dece4ec53d34f10eb5a113dde4
Mon Inson <66734118+monaxx@users.noreply.github.com>
2023-08-10 18:01:52 +0800

    Add basic boilerplate calculator functions

---
+++ js/myJavaScript.js
@@ -0,0 +1,13 @@
+
+function add(firstNum, operator, secondNum){
+    //
+}
+function subtract(firstNum, operator, secondNum){
+    //
+}
+function multiply(firstNum, operator, secondNum){
+    //
+}
+function divide(firstNum, operator, secondNum){
+    //
+}
