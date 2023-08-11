3d8db161e6b778d95a2b9d059f8f2d60ae295553
Mon Inson <66734118+monaxx@users.noreply.github.com>
2023-08-11 22:59:17 +0800

    Chain multiple math operations
    Need fix on:
    -Wrong answer when current operator is replaced with a new one

--- js/myJavaScript.js
+++ js/myJavaScript.js
@@ -1,33 +1,73 @@
 
-let firstNum, operator, secondNum;
-
+let firstNum, secondNum;
+let currentNum = "";
+let prevOperator = "", currOperator = "";
+const validOps = ['+', '-', '/', '*', '%'];
 
 function add(){
-    console.log("add");
+    return firstNum + secondNum;
 }
 function subtract(){
-    console.log("subtract");
+    return firstNum - secondNum;
 }
 function multiply(){
-    console.log("multiply");
+    return firstNum * secondNum;
 }
 function divide(){
-    console.log("divide");
+    return firstNum / secondNum;
+}
+function modulo(){
+    return firstNum % secondNum;
 }
 
 function operate(operator){
     switch(operator){
         case '+':
-            add();
-            break;
+            return add();
+     
         case '-':
-            subtract();
-            break;
+            return subtract();
+          
         case '*':
-            multiply();
-            break;
+            return multiply();
+       
         case '/':
-            divide();
-            break;  
+            return divide();
+            
+        case '%':
+            return modulo();
+             
     }    
 }
+
+let buttons = document.querySelectorAll(".calcButtonContainer > button");
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        let calcResultView = document.querySelector(".calcResultView");
+        let calcNewInputView = document.querySelector(".calcNewInputView");
+        if(!isNaN(button.textContent)){
+            currentNum += button.textContent;
+            currentNum  = +currentNum;
+            calcNewInputView.textContent = currentNum;
+        }else if(validOps.includes(button.textContent) && currentNum  !== ""){//Enter if valid math operator
+            if(firstNum == undefined){
+                currOperator = button.textContent;
+                console.log("first");
+                firstNum = currentNum ;
+                currentNum = "";
+                prevOperator = currOperator;
+            }else if(currentNum !== ""){
+                currOperator = button.textContent;
+                console.log("second");
+                secondNum = currentNum;
+                firstNum = operate(prevOperator);
+                currentNum = "";
+                prevOperator = currOperator;
+            }
+            calcResultView.textContent = firstNum + " " + currOperator;
+            calcNewInputView.textContent = "";
+            
+        }
+    });
+});
